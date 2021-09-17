4ee521fc6b524a20e0a5a20cce306445a96b6f06
unknown <dleitedias@gmail.com>
2021-09-16 21:37:35 -0300

    [Feature Deborah - Task: security concluded]

--- src/main/java/com/pessoalBlog/pessoalBlog/controller/UsuarioController.java
+++ src/main/java/com/pessoalBlog/pessoalBlog/controller/UsuarioController.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,7 +18,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.pessoalBlog.pessoalBlog.model.Usuario;
+import com.pessoalBlog.pessoalBlog.model.UsuarioLogin;
 import com.pessoalBlog.pessoalBlog.repository.UsuarioRepository;
+import com.pessoalBlog.pessoalBlog.service.UserService;
 
 @RestController
 @RequestMapping("/usuario")
@@ -26,6 +29,9 @@ public class UsuarioController {
 
 	@Autowired
 	private UsuarioRepository repository;
+	
+	@Autowired
+	private UserService usuarioService;
 	
 	@GetMapping("/todosUsuarios")
 	public ResponseEntity<List<Usuario>> getAll(){
@@ -51,10 +57,10 @@ public class UsuarioController {
 		}
 	}
 	
-	@PostMapping("/criarUsuario")
+	/*@PostMapping("/criarUsuario")
 	public ResponseEntity<Usuario> criarUsuario (@Valid @RequestBody Usuario novoUsuario){
 		return ResponseEntity.status(201).body(repository.save(novoUsuario));
-	}
+	}*/
 	
 	@PostMapping("/atualizarUsuario")
 	public ResponseEntity<Usuario> atualizarUsuario (@Valid @RequestBody Usuario atualizacaoUsuario){
@@ -63,5 +69,18 @@ public class UsuarioController {
 	@DeleteMapping("/deletarUsuario/{idUsuario}")
 	public void deletarPostagem ( @PathVariable(value = "idUsuario") Long idUsuario){
 		repository.deleteById(idUsuario);
+	}
+	
+	@PostMapping("/logar")
+	public ResponseEntity<UsuarioLogin> Autentication(@Valid @RequestBody Optional<UsuarioLogin> user) {
+		return usuarioService.Logar(user)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.status(HttpStatus.UNAUTHORIZED).build());
+	}
+
+	@PostMapping("/cadastrar")
+	public ResponseEntity<Usuario> Post(@RequestBody Usuario usuario) {
+		return ResponseEntity.status(HttpStatus.CREATED)
+				.body(usuarioService.CadastrarUsuario(usuario));
 	}
 }
